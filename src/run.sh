bef2aa6c696a0793123c5bab01c2d5bf094a31f1
chLi <565353780@qq.com>
2021-12-27 16:24:08 +0800

    update code format

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,10 @@
 python demo.py ctdet --demo ../images/16004479832_a748d55f21_k.jpg --load_model ../models/ctdet_coco_hg.pth --arch hourglass
+
+# Object Detect
+# For Image or Video
+# python demo.py ctdet --demo /path/to/image/or/folder/or/video --load_model ../models/ctdet_coco_dla_2x.pth
+# For WebCam
+# python demo.py ctdet --demo webcam --load_model ../models/ctdet_coco_dla_2x.pth
+
+# MultiPose
+# python demo.py multi_pose --demo /path/to/image/or/folder/or/video/or/webcam --load_model ../models/multi_pose_dla_3x.pth
